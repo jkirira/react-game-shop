5adcba49f9115cd64a361af1fde18dbb96b516d1
jkirira <jkirira@cytonn.com>
2023-02-16 12:13:42 +0300

    Adding react-router and home route.

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import browserRouter from './router'
+import './index.scss'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={browserRouter} />
   </React.StrictMode>,
 )
