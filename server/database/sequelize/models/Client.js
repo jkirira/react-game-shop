958eb825cbb036c4a47db45279e05a3fc8698a42
jkirira <jkirira@cytonn.com>
2023-03-02 21:25:03 +0300

    Add email confirmation token fields to User and Client Models. Remove email field.

--- server/database/sequelize/models/Client.js
+++ server/database/sequelize/models/Client.js
@@ -8,11 +8,14 @@ const Client = sequelize.define('Client', {
         primaryKey: true
     },
     username: DataTypes.STRING,
-    email: DataTypes.STRING,
     password: DataTypes.STRING,
     password_reset_token: DataTypes.STRING,
     password_reset_token_expiry: DataTypes.DATE,
-
+    password_reset_sent: DataTypes.TINYINT,
+    email_confirmation_token: DataTypes.STRING,
+    email_confirmation_token_expiry: DataTypes.DATE,
+    email_confirmation_sent: DataTypes.TINYINT,
+    registration_complete: DataTypes.TINYINT,
 }, 
 {
     tableName: 'clients'
--- server/database/sequelize/models/User.js
+++ server/database/sequelize/models/User.js
@@ -8,10 +8,14 @@ const User = sequelize.define('User', {
         primaryKey: true
     },
     username: DataTypes.STRING,
-    email: DataTypes.STRING,
     password: DataTypes.STRING,
     password_reset_token: DataTypes.STRING,
     password_reset_token_expiry: DataTypes.DATE,
+    password_reset_sent: DataTypes.TINYINT,
+    email_confirmation_token: DataTypes.STRING,
+    email_confirmation_token_expiry: DataTypes.DATE,
+    email_confirmation_sent: DataTypes.TINYINT,
+    registration_complete: DataTypes.TINYINT,
 }, 
 {
     tableName: 'users'
