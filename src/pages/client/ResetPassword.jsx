6969f877abb46c24d9ee30ea09dfd6f000841bba
jkirira <jkirira@cytonn.com>
2023-04-08 23:32:12 +0300

    Refactor client side reset password controller to pass the token instead of the email.

--- src/pages/client/ResetPassword.jsx
+++ src/pages/client/ResetPassword.jsx
@@ -50,7 +50,7 @@ export default function ResetPassword() {
             return false;
         }
 
-        form_data['email'] = email;
+        form_data['token'] = password_reset_token;
         form_data['password'] = password;
         // form_data['password_confirmation'] = passwordConfirmation
         
