6eb1a290c0fb9393f7114063d6ad7ee53e572c0a
jkirira <jkirira@cytonn.com>
2023-04-16 10:51:46 +0300

    Seperating admin and client navbars.

--- src/layouts/BaseLayout.jsx
+++ src/layouts/BaseLayout.jsx
@@ -1,16 +1,9 @@
 import { Outlet } from 'react-router-dom'
-import Navbar from '../components/client/Navbar';
 
 export default function BaseLayout() {
     return (
-        <>
-
-            <Navbar />
-
-            <div className="main">
-                <Outlet />
-            </div>
-
-        </>
+        <div className="main">
+            <Outlet />
+        </div>
     );
 }
