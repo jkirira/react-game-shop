74e1d6ff40ddfbbf0a41150007e1a27f5df52af5
jkirira <jkirira@cytonn.com>
2023-04-30 13:20:49 +0300

    install and uninstall error if missing env file.

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # COMPOSE_PROJECT_NAME=react_game_shop_project
 
+if [ ! -f ./docker/.env ]; then
+    echo "\nfile not found... \"$(pwd)/docker/.env\" \n"
+    echo "Please set environment variables in \"$(pwd)/docker/.env\""
+    echo "See \"$(pwd)/docker/.env.example\" \n"
+    exit 1
+fi
+
 source ./docker/.env
 
 echo "Please wait while service is being destroyed..." \
